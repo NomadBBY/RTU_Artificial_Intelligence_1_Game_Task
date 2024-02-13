8238691233594bcead36ef29c4be023c138417b7
Artis_Code <89861525+NomadBBY@users.noreply.github.com>
2024-02-13 17:17:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.locals import *
 
-# Define colors and constants
 class Colours:
     
     BLACK = (0, 0, 0)
@@ -11,28 +10,54 @@ class Colours:
     BLUE = (0, 0, 255)
 
 class Constants:
-
+    
     SQUARE_SIZE = 100
-    SCREEN_SIZE = (600, 600)
+    SCREEN_SIZE = (700, 700)
 
-# Function to draw squares
-def draw_squares(game_screen, colors, square_size):
+class BoardSetup:
+    
+    def __init__(self, game_screen) -> None:
+        self.game_screen = game_screen
 
-    game_screen.fill(colors.BLACK)
-    for row in range(8):
-        for col in range(row % 2, 8, 2):
-            x = col * square_size
-            y = row * square_size
-            square_rect = pygame.Rect(x, y, square_size, square_size)
-            pygame.draw.rect(game_screen, colors.RED, square_rect)
+    def draw_squares(self):
+        self.game_screen.fill(Colours.BLACK)
+        for row in range(8):
+            for col in range(row % 2, 8, 2):
+                x = col * Constants.SQUARE_SIZE
+                y = row * Constants.SQUARE_SIZE
+                square_rect = pygame.Rect(x, y, Constants.SQUARE_SIZE, Constants.SQUARE_SIZE)
+                pygame.draw.rect(self.game_screen, Colours.WHITE, square_rect)
 
+    @staticmethod
+    def get_row_from_mouse(position):
+        x_cord, y_cord = position
+        row_index = x_cord // Constants.SQUARE_SIZE
+        col_index = y_cord // Constants.SQUARE_SIZE
+        return row_index, col_index
+    
 
+    class GameBoard():
 
+        def __init__(self):
+            # Initialize instance variables to track the game state
+            self.game_board = []  # List containing the game board
+            self.red_pieces_remaining = 12  # Number of remaining red pieces
+            self.white_pieces_remaining = 12  # Number of remaining white pieces
+            self.num_red_kings = 0  # Number of red kings
+            self.num_white_kings = 0  # Number of white kings
+            self.create_board()
+
+        def calculate_score(self, king_weight=0.5):  
+            #  Aprēķina rezultātu, pamatojoties uz atšķirību starp katras krāsas gabalu skaitu un karaļa figūru skaitu
+            return self.white_pieces_remaining - self.red_pieces_remaining + (self.num_white_kings * king_weight - self.num_red_kings * king_weight)
 
 
 # Initialize pygame
 pygame.init()
 game_screen = pygame.display.set_mode(Constants.SCREEN_SIZE)
+
+# Create a board instance
+board = BoardSetup(game_screen)
 
 # Main loop
 running = True
@@ -41,7 +66,7 @@ while running:
         if event.type == QUIT:
             running = False
 
-    draw_squares(game_screen, Colours, Constants.SQUARE_SIZE)
+    board.draw_squares()
     pygame.display.flip()
 
 # Quit pygame
