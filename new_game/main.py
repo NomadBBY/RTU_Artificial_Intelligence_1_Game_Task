7e8887d99deadedc4729a30bb495da3fe3037a5a
Artis_Code <89861525+NomadBBY@users.noreply.github.com>
2024-04-03 12:50:07 +0300

    Added placeholder for Alfa-Beta

--- new_game/main.py
+++ new_game/main.py
@@ -7,7 +7,7 @@ BUTTON_COLOR = 100, 100, 100
 
 class Algorithm:
     '''
-    Implementation of the MiniMax algorithm for the game
+    Implementation of the MiniMax algorithm with Alpha-Beta pruning for the game
     '''
 
     def __init__(self) -> None:
@@ -26,27 +26,45 @@ class Algorithm:
                 return score2 - score1, None
 
         if maximizing_player:
-            max_eval = float('-inf')
-            best_move = None
-            for move in self.get_possible_moves(number1, turn):
-                new_number1 = self.make_move(number1, move)
-                new_score1 = self.update_score(score1, new_number1)
-                evaluation = self.minimax(new_number1, number2, new_score1, score2, 'computer', depth - 1, False)[0]
-                if evaluation > max_eval:
-                    max_eval = evaluation
-                    best_move = move
-            return max_eval, best_move
+            return self.max_value(number1, number2, score1, score2, turn, depth, float('-inf'), float('inf'))
         else:
-            min_eval = float('inf')
-            best_move = None
-            for move in self.get_possible_moves(number2, turn):
-                new_number2 = self.make_move(number2, move)
-                new_score2 = self.update_score(score2, new_number2)
-                evaluation = self.minimax(number1, new_number2, score1, new_score2, 'human', depth - 1, True)[0]
-                if evaluation < min_eval:
-                    min_eval = evaluation
-                    best_move = move
-            return min_eval, best_move
+            return self.min_value(number1, number2, score1, score2, turn, depth, float('-inf'), float('inf'))
+
+    def max_value(self, number1, number2, score1, score2, turn, depth, alpha, beta):
+        if depth == 0 or number1 >= 1000 or number2 >= 1000:
+            return score2 - score1, None
+
+        max_eval = float('-inf')
+        best_move = None
+        for move in self.get_possible_moves(number1, turn):
+            new_number1 = self.make_move(number1, move)
+            new_score1 = self.update_score(score1, new_number1)
+            evaluation = self.min_value(new_number1, number2, new_score1, score2, 'computer', depth - 1, alpha, beta)[0]
+            if evaluation > max_eval:
+                max_eval = evaluation
+                best_move = move
+            alpha = max(alpha, max_eval)
+            if beta <= alpha:
+                break
+        return max_eval, best_move
+
+    def min_value(self, number1, number2, score1, score2, turn, depth, alpha, beta):
+        if depth == 0 or number1 >= 1000 or number2 >= 1000:
+            return score2 - score1, None
+
+        min_eval = float('inf')
+        best_move = None
+        for move in self.get_possible_moves(number2, turn):
+            new_number2 = self.make_move(number2, move)
+            new_score2 = self.update_score(score2, new_number2)
+            evaluation = self.max_value(number1, new_number2, score1, new_score2, 'human', depth - 1, alpha, beta)[0]
+            if evaluation < min_eval:
+                min_eval = evaluation
+                best_move = move
+            beta = min(beta, min_eval)
+            if beta <= alpha:
+                break
+        return min_eval, best_move
 
     def get_possible_moves(self, number, turn):
         if turn == 'computer':
