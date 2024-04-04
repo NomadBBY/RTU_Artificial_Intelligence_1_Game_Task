c486b0740f8a5fffbfa2c6c4ece0bbec2f657f85
Artis_Code <89861525+NomadBBY@users.noreply.github.com>
2024-04-04 11:33:00 +0300

    Update algo_testing.py

--- new_game/algo_testing.py
+++ new_game/algo_testing.py
@@ -18,6 +18,39 @@ class Algorithm:
             return [2, 3]
         else:
             return list(range(2, min(number, 6) + 1))
+        
+    def generate_game_tree(self, number1, number2, score1, score2, turn, depth):
+        if depth == 0 or number1 >= 1000 or number2 >= 1000:
+            return None, self.evaluate(number1, number2, score1, score2)
+        
+        if turn == 'computer':
+            maximizing_player = True
+            best_value = float('-inf')
+            best_move = None
+            for move in self.get_possible_moves(number1, turn):
+                new_number1 = self.make_move(number1, move)
+                new_score1 = self.update_score(score1, new_number1)
+                _, value = self.generate_game_tree(new_number1, number2, new_score1, score2, 'human', depth - 1)
+                if value > best_value:
+                    best_value = value
+                    best_move = move
+            return best_move, best_value
+        else:
+            maximizing_player = False
+            best_value = float('inf')
+            best_move = None
+            for move in self.get_possible_moves(number2, turn):
+                new_number2 = self.make_move(number2, move)
+                new_score2 = self.update_score(score2, new_number2)
+                _, value = self.generate_game_tree(number1, new_number2, score1, new_score2, 'computer', depth - 1)
+                if value < best_value:
+                    best_value = value
+                    best_move = move
+            return best_move, best_value
+
+    def evaluate(self, number1, number2, score1, score2):
+        # This is a simple heuristic function to evaluate leaf nodes
+        return score2 - score1  # Difference in scores
 
 
 class MinimaxAlgorithm(Algorithm):
